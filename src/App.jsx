8f5a87153827de397440f043d9c7939fb8ff823c
helder mabasso <hfmabasso@gmail.com>
2024-06-12 01:56:59 +0200

    Route added for homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navigation from './navbar'
 import './App.css'
+import HomePage from './homepage'
 
 
 
 function App() {
 
-
-
      return (
-      <BrowserRouter>
-      
+      <BrowserRouter>    
         <Navigation/>
-
-
-
+        <Routes>
+          <Route path='/homepage' element={<HomePage/>}></Route>
+        </Routes>
       </BrowserRouter>
   )
   }
