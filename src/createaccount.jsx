ba70f64ce7888e8f44b5a31e0183e2c266f74813
helder mabasso <hfmabasso@gmail.com>
2024-06-17 01:53:31 +0200

    updating form

--- src/createaccount.jsx
+++ src/createaccount.jsx
@@ -1,49 +1,101 @@
-const CreateAccount = ()=>{
-    
-    return(
-    
+import { useFormik } from "formik";
+
+const CreateAccount = () => {
+    const formik = useFormik({
+        initialValues: {
+            firstName: '',
+            lastName: '',
+            email: '',
+            address: '',
+            password: ''
+        },
+        onSubmit: values => {
+            alert(`The account was successfully created`);
+            // You can handle form submission logic here
+        },
+    });
+
+    return (
         <div className="card text-center w-100 p-3">
             <div className="card-header">
                 <h5 className="card-title">Create Account</h5>
             </div>
-            <form className="card-body">
-
+            <form className="card-body" onSubmit={formik.handleSubmit}>
                 <div className="row gx-5 align-items-center form-floating">
-                    
-                    <div className="col-auto">
-                        <input type="name" className="form-control" placeholder="Name" id="name" aria-describedby="nameHelp"/>
+                    <div className="col">
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="firstName"
+                            name="firstName"
+                            placeholder="First Name"
+                            value={formik.values.firstName}
+                            onChange={formik.handleChange}
+                        />
                     </div>
 
-                   
-                    <div className="col-auto">
-                        <input type="name" className="form-control" id="name" placeholder="Last Name" aria-describedby="nameHelp"/>
-                    </div>
-
-                </div>
-
-                <br/>
-
-                <div class="form-floating mb-3">
-                    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"/>
-                    <label for="floatingInput">Email address</label>
-                </div>
-
-                <div class="form-floating mb-3">
-                    <input type="text" class="form-control" id="floatingInput" placeholder="Address"/>
-                    <label for="floatingInput">Address</label>
-                </div>
+                    <div className="col">
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="lastName"
+                            name="lastName"
+                            placeholder="Last Name"
+                            value={formik.values.lastName}
+                            onChange={formik.handleChange}
+                        />
                 
-                <div class="form-floating">
-                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password"/>
-                    <label for="floatingPassword">Password</label>
-                </div>                
-                <br/>
-                <button type="submit" className="btn btn-success">Create Account</button>
+                    </div>
+                </div>
+
+                <br />
+
+                <div className="form-floating mb-3">
+                    <input
+                        type="email"
+                        className="form-control"
+                        id="email"
+                        name="email"
+                        placeholder="name@example.com"
+                        value={formik.values.email}
+                        onChange={formik.handleChange}
+                    />
+                    <label htmlFor="email">Email address</label>
+                </div>
+
+                <div className="form-floating mb-3">
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="address"
+                        name="address"
+                        placeholder="Address"
+                        value={formik.values.address}
+                        onChange={formik.handleChange}
+                    />
+                    <label htmlFor="address">Address</label>
+                </div>
+
+                <div className="form-floating">
+                    <input
+                        type="password"
+                        className="form-control"
+                        id="password"
+                        name="password"
+                        placeholder="Password"
+                        value={formik.values.password}
+                        onChange={formik.handleChange}
+                    />
+                    <label htmlFor="password">Password</label>
+                </div>
+
+                <br />
+                <button type="submit" className="btn btn-success">
+                    Create Account
+                </button>
             </form>
         </div>
-    
-
-    )
-}
+    );
+};
 
 export default CreateAccount;
