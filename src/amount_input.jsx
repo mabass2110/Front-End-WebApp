322d88df6cb967c9ada08caefc9be669964d240c
helder mabasso <hfmabasso@gmail.com>
2024-07-02 02:04:13 +0200

    modifying balance format

--- src/amount_input.jsx
+++ src/amount_input.jsx
@@ -3,7 +3,7 @@ function Input({handleSubmit, operation, handleOperation, handleChange, value, b
         <>
         <div className="card mb-3" >
            <div className="card-body">
-               <h5 className="card-title text-start">Balance:{balance} $USD</h5>
+               <h5 className="card-title text-start">Balance:${balance} USD</h5>
                 <form onSubmit={handleSubmit}>
                    <div className="balance-input d-flex align-items-center">
                        <input type="text" 
@@ -12,7 +12,7 @@ function Input({handleSubmit, operation, handleOperation, handleChange, value, b
                        onChange={handleChange}
                        placeholder={`Enter ${operation} amount`}
                        name="amount"/> 
-                       <button type="submit" disabled={disabled} className="btn btn-primary" onClick={handleOperation}>Deposit</button>
+                       <button type="submit" disabled={disabled} className="btn btn-primary" onClick={handleOperation}>{operation}</button>
                    </div>
                    
                  
