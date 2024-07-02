b5656abbcc0122316a8749367a57abb23ffbf4a2
helder mabasso <hfmabasso@gmail.com>
2024-07-02 07:09:13 +0200

    comments

--- src/alldataprovider.jsx
+++ src/alldataprovider.jsx
@@ -4,25 +4,28 @@ import AllDataContext from "./alldata_context";
 
 const AllDataProvider = ({children}) => {
 
+  //create account state
     const [firstName,setFirstName] = useState('')
     const [lastName,setLastName] = useState('')
     const [email,setEmail]= useState('');
     const [address,setAddress] = useState('')
-
+     
     const setFirst = (firstName) => setFirstName(firstName)
     const setLast = (lastName) => setLastName(lastName)
     const setMail = (email) => setEmail(email)
     const setAddr = (address) => setAddress(address)
     
-    const [balance, setBalance] = useState(15000); // State for account balance
+    //balance state
+    const [balance, setBalance] = useState(15000); 
 
     const deposit = (amount) => {
       setBalance(prevBalance => prevBalance + amount);
     };
   
     const withdraw = (amount) => {
-      setBalance(prevBalance => Math.max(0, prevBalance - amount)); // Prevent negative balance
+      setBalance(prevBalance => Math.max(0, prevBalance - amount)); 
     };
+    
 
     
 
