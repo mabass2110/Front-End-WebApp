5ddda4661c7c3ca3f67ca2b204d0ceb402dbcc8e
helder mabasso <hfmabasso@gmail.com>
2024-07-02 07:10:05 +0200

    adding validation

--- src/deposit.jsx
+++ src/deposit.jsx
@@ -10,8 +10,10 @@ import AllDataContext from "./alldata_context";
 const Deposit = () => {
     const [disable, setDisable] = useState(true);
     const [depositAmount, setDepositAmount] = useState('')
+    const [valid, setValid] = useState(false)
+    const [invalid,setInvalid] = useState(false)
     const {balance, deposit} = useContext(AllDataContext)
-
+  
     const handleSubmit = (e)=>{  
         e.preventDefault() 
     }
@@ -19,16 +21,24 @@ const Deposit = () => {
     const handleDeposit = (e)=> {
        
        if(isNaN(depositAmount)){
+            setInvalid(true);
+            setValid(false);
             alert('Please add a number!')
+            
         }
         else if(depositAmount <= 0){
+            setInvalid(true);
+            setValid(false);
             alert('Please insert a positive deposit amount!');
         }
         else{
             deposit(Number(depositAmount))
+            setValid(true)
+            setInvalid(false)
         }
         setDepositAmount('') 
         setDisable(true)
+        
     }
 
     const handleChange = (e)=>{
@@ -40,7 +50,16 @@ const Deposit = () => {
     
     return(
         <> 
-            <Input handleSubmit={handleSubmit} operation='deposit'  handleOperation={handleDeposit} handleChange={handleChange} value={depositAmount} balance={balance} disabled={disable}/>
+            <Input handleSubmit={handleSubmit} 
+                   operation='deposit' 
+                   handleOperation={handleDeposit} 
+                   handleChange={handleChange} 
+                   value={depositAmount} 
+                   balance={balance} 
+                   disabled={disable}
+            />
+            {valid && <div class="alert alert-success" role="alert"> Deposit Successful!</div>}
+            {invalid && <div class="alert alert-danger" role="alert"> Error: Unable to Process Deposit!</div>}
         </>
     )
 }
